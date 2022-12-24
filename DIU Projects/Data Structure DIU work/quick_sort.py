19106f82cc8f1c24af11173362de7c48b2daa5d5
Pushon <mh.imranhossain@outlook.com>
2022-12-24 12:05:25 +0600

    fixed error

--- DIU Projects/Data Structure DIU work/quick_sort.py
+++ DIU Projects/Data Structure DIU work/quick_sort.py
@@ -1,40 +1,56 @@
-def partition(arr, low, high):
-    i = low - 1
-    pivot = arr[high]
-    if x == 1:
-        for j in range(low, high):
-            if arr[j] < pivot:
-                i = i + 1
-                arr[i], arr[j] = arr[j], arr[i]
-        arr[i + 1], arr[high] = arr[high], arr[i + 1]
-        return i + 1
-    elif x == 2:
-        for j in range(low, high):
-            if arr[j] > pivot:
-                i = i + 1
-                arr[i], arr[j] = arr[j], arr[i]
-        arr[i + 1], arr[high] = arr[high], arr[i + 1]
-        return i + 1
-    else:
-        print("---Error---")
+# Quick sort in Python
+
+# function to find the partition position
+def partition(array, low, high):
+
+  # choose the rightmost element as pivot
+  pivot = array[high]
+
+  # pointer for greater element
+  i = low - 1
+
+  # traverse through all elements
+  # compare each element with pivot
+  for j in range(low, high):
+    if array[j] <= pivot:
+      # if element smaller than pivot is found
+      # swap it with the greater element pointed by i
+      i = i + 1
+
+      # swapping element at i with element at j
+      (array[i], array[j]) = (array[j], array[i])
+
+  # swap the pivot element with the greater element specified by i
+  (array[i + 1], array[high]) = (array[high], array[i + 1])
+
+  # return the position from where partition is done
+  return i + 1
+
+# function to perform quicksort
 
 
-def quickSort(arr, low, high):
-    if low < high:
-        pi = partition(arr, low, high)
+def quickSort(array, low, high):
+  if low < high:
 
-        quickSort(arr, low, pi - 1)
-        quickSort(arr, pi + 1, high)
+    # find pivot element such that
+    # element smaller than pivot are on the left
+    # element greater than pivot are on the right
+    pi = partition(array, low, high)
+
+    # recursive call on the left of pivot
+    quickSort(array, low, pi - 1)
+
+    # recursive call on the right of pivot
+    quickSort(array, pi + 1, high)
 
 
-arr = []
-inp = int(input("Enter the Number of items and the list: "))
-for i in range(inp):
-    p = int(input())
-    arr.append(p)
-x = int(input("for ascending order press 1\n for descending order press 2\n>>>"))
-n = len(arr)
-quickSort(arr, 0, n - 1)
-print("Sorted List is: ")
-for i in range(n):
-    print(arr[i], end=" ")
+data = [8, 7, 2, 1, 0, 9, 6]
+print("Unsorted Array")
+print(data)
+
+size = len(data)
+
+quickSort(data, 0, size - 1)
+
+print('Sorted Array in Ascending Order:')
+print(data)
